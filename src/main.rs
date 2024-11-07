aef77829f18ae160f11d5a077b900a821a09dd26
Guille <guillermobernallou@gmail.com>
2024-11-07 13:46:56 +0100

    Refactor scraping functions to write reviews and ratings to CSV file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use reqwest;
 use scraper::{Html, Selector};
+use std::fs::File;
+use std::io::Write;
 
 struct Movie {
     id: i32,
@@ -16,7 +18,7 @@ struct Rating {
 
 const URL_PREFIX: &str = "https://www.filmaffinity.com/es/reviews/";
 
-fn scrap_review_page(id: i32, page: u8) {
+fn scrap_review_page(id: i32, page: u8, csv_writer: &mut csv::Writer<File>) {
     let url = format!("{}{}/{}.html", URL_PREFIX, page, id);
     let response = reqwest::blocking::get(&url).unwrap();
     if response.status().is_success() {
@@ -37,16 +39,20 @@ fn scrap_review_page(id: i32, page: u8) {
 
         for rating in ratings {
             println!("Rating: {:?}, Review: {:?}", rating.rating, rating.review);
+            csv_writer.write_record(&[
+                rating.review.clone().unwrap_or_default(),
+                rating.rating.map(|r| r.to_string()).unwrap_or_default(),
+            ]).unwrap();
         }
     } else {
         println!("Failed to fetch the review page: {}", url);
     }
 }
 
-fn scrap_film(id: i32, pages: u8) {
+fn scrap_film(id: i32, pages: u8, csv_writer: &mut csv::Writer<File>) {
     if pages > 0 {
         for i in 0..pages {
-            scrap_review_page(id, i + 1);
+            scrap_review_page(id, i + 1, csv_writer);
         }
     } else {
         println!("No pages to scrape for film ID: {}", id);
@@ -60,8 +66,13 @@ fn get_pages(id: i32) -> u8 {
         let data = response.text().unwrap();
         let page_selector = Selector::parse("div.pager a").unwrap();
         let document = Html::parse_document(&data);
-        if let Some(last_page) = document.select(&page_selector).last() {
-            return last_page.text().collect::<Vec<_>>().join(" ").trim().parse::<u8>().unwrap_or(1);
+        let mut pages = document.select(&page_selector).collect::<Vec<_>>();
+        if pages.len() > 1 {
+            if let Some(second_last_page) = pages.iter().nth_back(1) {
+                let second_last_page_text = second_last_page.text().collect::<Vec<_>>().join(" ").trim().to_string();
+                println!("Second to last page: {}", second_last_page_text);
+                return second_last_page_text.parse::<u8>().unwrap_or(1);
+            }
         }
     }
     1
@@ -91,7 +102,9 @@ fn get_top_films(movies: &mut Vec<Movie>) {
 fn main() {
     let mut movies: Vec<Movie> = Vec::new();
     get_top_films(&mut movies);
+    let mut csv_writer = csv::Writer::from_path("reviews.csv").unwrap();
+    csv_writer.write_record(&["Review", "Rating"]).unwrap();
     for movie in &movies {
-        scrap_film(movie.id, movie.pages);
+        scrap_film(movie.id, movie.pages, &mut csv_writer);
     }
 }
